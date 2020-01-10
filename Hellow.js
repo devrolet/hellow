21001cd5e895eaade4c32d905d7147f970dacb37
devrolet <chetdhill@gmail.com>
2020-01-10 13:20:16 -0500

    Set up Safe Coding Space (IIFE)

---
+++ Hellow.js
@@ -0,0 +1,11 @@
+/* 
+*  Hellow.js JavaScript Library v.1.0.0
+*  Released under the MIT license
+*  Includes jQuery
+*  Copyright 2020 Chet Hill.
+*/
+(function(global, $) {
+
+  
+
+}(window, jQuery));
